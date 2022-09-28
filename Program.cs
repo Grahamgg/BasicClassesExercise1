af8803114e405cb6631e3f1dd1cc1b1ad14e59e8
Graham Galloway <Grahamgalloway0@gmail.com>
2022-09-28 15:59:22 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses_Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var prius = new Car();
+            prius.Make = "Toyota";
+            prius.Model = "Prius";
+            prius.Year = 2006;
+
+  
+
+
+            //Console.WriteLine(prius.Make);
+            //Console.WriteLine(prius.Model);
+            //Console.WriteLine(prius.Year);
+
+            var mazda = new Car()
+            {
+             Make = "Mazda",
+            Model = "CX5",
+            Year = 2015,
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { prius,mazda, chevy };
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+            
+          
+            
+            
+            
+
+
+
+        }
+
+    }
+}
